56a000445be839931a0d62f21f21a0aabaf4dc93
tayuanfang <tayuanfang@foxmail.com>
2017-06-01 17:03:28 +0800

    widget project example; widget example.

---
+++ ionic/examples/widget/index.js
@@ -0,0 +1,11 @@
+/**
+ * Created by Administrator on 2017/6/1.
+ */
+angular.module('ionicApp', ['ionic'])
+.controller('appCtrl', function($scope, $http) {
+        //$http.get("/elements/buttonlist.html").then(function(response) {
+        //    $scope.buttonlist = response.data;
+        //}, function(response) {
+        //    $scope.buttonlist = '<b style="color: red">Http get is error!</b>';
+        //});
+    });
